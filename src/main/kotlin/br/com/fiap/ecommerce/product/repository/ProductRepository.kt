c7e18debede85022bd7dc562252612f927c41524
Kayky_Schunck <107579267+KaykySchunck@users.noreply.github.com>
2025-09-21 09:31:06 -0300

    Update - Injeção proposital ProductRepository.kt
    //Injeção de Sql para teste de código - proposital

--- src/main/kotlin/br/com/fiap/ecommerce/product/repository/ProductRepository.kt
+++ src/main/kotlin/br/com/fiap/ecommerce/product/repository/ProductRepository.kt
@@ -9,5 +9,8 @@ import java.util.UUID
 
 @Repository
 interface ProductRepository : JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
+//Injeção de Sql para teste de código - proposital
+    @Query("SELECT * FROM product WHERE name = '\" || :name || \"'", nativeQuery = true)
+    fun findByNameUnsafe(@Param("name") name: String): List<Product>
 
 }
